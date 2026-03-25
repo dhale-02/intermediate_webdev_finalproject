7a1c9a3dd40a0cbfb73b08492e5af1f7438b6533
dhale-02 <dhale.caparros@caparrosui.com>
2026-03-25 23:05:37 +0800

    Update calculateSpec.js

--- spec/calculateSpec.js
+++ spec/calculateSpec.js
@@ -1,9 +1,8 @@
-const { calculate } = require("../src/script.js");
+const { calculate } = require('../src/script.js');
 
 describe("calculate", function () {
 
   it("should return correct simple interest for valid inputs", function () {
-    // 1000 * (5/100) * 2 = 100
     expect(calculate(1000, 5, 2)).toBe(100);
   });
 
